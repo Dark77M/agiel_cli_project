433be2d3b8ec97bfcdcf074bc03c70edb07bbb97
Dark77M <Alihbek@outlook.com>
2023-12-04 13:53:07 +0100

    commit first

---
+++ src/main/java/it/unical/agilecliproject/AgileCliProjectApplication.java
@@ -0,0 +1,13 @@
+package it.unical.agilecliproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileCliProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileCliProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unical/agilecliproject/AgileCliProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.agilecliproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileCliProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
