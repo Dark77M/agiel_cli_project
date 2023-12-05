e5fc188f4d19ebd651197dfb25138bec399098cf
Dark77M <Alihbek@outlook.com>
2023-12-05 17:34:42 +0100

    commit Done export

--- src/main/java/it/unical/agilecliproject/commands/ProjectCommand.java
+++ src/main/java/it/unical/agilecliproject/commands/ProjectCommand.java
@@ -1,10 +1,18 @@
 package it.unical.agilecliproject.commands;
 
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import it.unical.agilecliproject.utilities.Commons;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 @ShellComponent
 public class ProjectCommand {
@@ -40,4 +48,64 @@ public class ProjectCommand {
         isActivityStarted = false;
         activityName = "";
     }
+    @ShellMethod(key = "export",value = "export log files into csv format")
+    public void exportData(
+            @ShellOption(value = {"-f","--format"})String format,
+            @ShellOption(value = {"-p","path"})String location){
+        if (format == null || format.isEmpty()){
+            System.out.println("Format is required");
+            return;
+        }
+        if (location==null||location.isEmpty()){
+            System.out.println("Location is required");
+            return;
+        }
+
+        try {
+            Path savePath = Paths.get(location);
+            Path loadPath = Paths.get("C:\\Users\\F\\IdeaProjects\\AgileCLIProject\\my_logs.txt");
+            if (format.equals("csv") || format.equals("CSV")) {
+                String header = "Date;Time;Event;Activity;";
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(header).append("\n");
+                List<String> strings = Files.readAllLines(loadPath);
+                for (String info : strings) {
+                    if (info.startsWith("INFO")) {
+                        String[] data = info.split(": ");
+                        stringBuilder.append(data[1]).append("\n");
+                    }
+                }
+                System.out.println(stringBuilder.toString());
+                Files.write(savePath,stringBuilder.toString().getBytes(),
+                        StandardOpenOption.CREATE_NEW,StandardOpenOption.TRUNCATE_EXISTING,StandardOpenOption.WRITE);
+
+
+            }else if (format.equals("json") || format.equals("JSON")) {
+                List<String> strings = Files.readAllLines(loadPath);
+                JsonArray jsonArray = new JsonArray();
+                for (String info : strings){
+                    if (info.startsWith("INFO")){
+                        String[] data = info.split(": ");
+                        String[] jsonObjectData = data[1].split(";");
+
+                        JsonObject object = new JsonObject();
+                        object.addProperty("date",jsonObjectData[0].trim());
+                        object.addProperty("time",jsonObjectData[1].trim());
+                        object.addProperty("event",jsonObjectData[2].trim());
+                        object.addProperty("activity",jsonObjectData[3].trim());
+
+                        jsonArray.add(object);
+                    }
+                }
+                Files.write(savePath,jsonArray.toString().getBytes(),
+                        StandardOpenOption.CREATE_NEW,StandardOpenOption.TRUNCATE_EXISTING,StandardOpenOption.WRITE);
+            }
+
+        }
+        catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+
+}
 }
